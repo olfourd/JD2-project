eda1251f18023a58caaa124f5065e8b426f98402
olfourd <olfourd@gmail.com>
2017-06-03 01:17:32 +0300

    3 недокинутых файла

---
+++ dao/src/main/java/by/itacademy/entity/HeroRole.java
@@ -0,0 +1,13 @@
+package by.itacademy.entity;
+
+/**
+ * Created by цифра on 01.06.2017.
+ */
+public enum HeroRole {
+    ASSASIN,
+    WARRIOR,
+    MAGE,
+    SHOOTER,
+    SUPPORT,
+    TANK;
+}
